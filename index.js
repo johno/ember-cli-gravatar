0b64df3ae2e12244684205524f1ed0e9000a294f
John Otander <johnotander@gmail.com>
2014-09-12 17:35:17 -0600

    Begin new implementation to auto import the md5 function.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+'use strict';
+
 module.exports = {
-  name: 'ember-cli-gravatar'
-}
+  name: 'ember-cli-gravatar',
+
+    included: function included(app) {
+    this.app = app;
+    this._super.included(app);
+
+    app.import("node_modules/blueimp-md5/md5.js");
+  }
+};
