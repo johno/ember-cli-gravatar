2cb79f8e65fb7415a91d16427c742f6d4e7c2932
John Otander <johnotander@gmail.com>
2014-10-20 12:23:08 -0600

    Bump version and fix blueprint.

--- blueprints/ember-cli-gravatar/index.js
+++ blueprints/ember-cli-gravatar/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function(options) {
-    this.addBowerPackageToProject('JavaScript-MD5');
+    return this.addBowerPackageToProject('JavaScript-MD5');
   }
 };
