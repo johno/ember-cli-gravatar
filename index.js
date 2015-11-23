a361c3f5c0ba434e4b126288ca4fc07f2f4cbdd0
John Otander <johnotander@gmail.com>
2015-11-23 13:38:45 -0700

    Use blueimp-md5

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
     this.app = app;
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/JavaScript-MD5/js/md5.js');
+    app.import(app.bowerDirectory + '/blueimp-md5/js/md5.js');
     app.import('vendor/ember-cli-gravatar/md5-shim.js', {
       type: 'vendor',
       exports: { 'md5': ['md5'] }
